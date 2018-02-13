e2ae2451c9a72fe399de384ffb53881295384ce1
Yannick <yannick1691@users.noreply.github.com>
2018-02-13 12:30:49 +0100

    Split up saga*

---
+++ src/sagas/photoSaga.js
@@ -0,0 +1,18 @@
+import { put, call } from 'redux-saga/effects';
+import { flickrImages } from '../Api/api';
+import * as types from '../constants/actionTypes';
+
+// Responsible for searching media library, making calls to the API
+// and instructing the redux-saga middle ware on the next line of action,
+// for success or failure operation.
+export function* searchPhotoSaga({ payload }) {
+  try {
+    const images = yield call(flickrImages, payload);
+    yield [
+      put({ type: types.FLICKR_IMAGES_SUCCESS, images }),
+      put({ type: types.SELECTED_IMAGE, image: images[0] })
+    ];
+  } catch (error) {
+    yield put({ type: 'SEARCH_MEDIA_ERROR', error });
+  }
+}
--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,8 +1,13 @@
 import { takeLatest } from 'redux-saga/effects';
-import { searchMediaSaga } from './mediaSaga';
+// import { searchMediaSaga } from './mediaSaga';
+import { searchPhotoSaga } from './photoSaga';
+import { searchVideoSaga } from './videoSaga';
 import * as types from '../constants/actionTypes';
 
 // Watches for SEARCH_MEDIA_REQUEST action type asynchronously
 export default function* watchSearchMedia() {
-  yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+  yield [
+    takeLatest(types.SEARCH_MEDIA_REQUEST, searchPhotoSaga),
+    takeLatest(types.SEARCH_MEDIA_REQUEST, searchVideoSaga)
+  ]
 }
